227cbca7bf182516e91fb8a789c5e771a418a631
Alexander-Serov <a.s.serov@gmail.com>
2018-05-30 15:46:27 +0200

    Updated 2D plots to vary with the perpendicular component

--- analytical_bayes_factor/main.py
+++ analytical_bayes_factor/main.py
@@ -32,86 +32,129 @@ zeta_sp_abs_lim = 1.00  # 0.02
 zeta_sp_steps = 100
 dim = 2
 n_pi = 5 - dim  # minimum number of jumps is different for priors in 1D and 2D
-ns = [100, 500]
 B = 10  # at least strong evidence against H0
 log10_B = np.log10(B)
-zeta_t_perp = 0.5  # total force component orthogonal to the diffusivity gradient
 
+lambs_count = len(lambs)
+
+# Define dimension-specific parameters
 # Relative prior uncertainties (u = Vp/V)
 us_1D = [0.1, 10.0]
 us_2D = [1.0]
+
+# total force component orthogonal to the diffusivity gradient
+zeta_t_perp_1D = [0.0]
+zeta_t_perp_2D = [0.0, 0.1, 0.2]
+
+# number of jumps
+ns_1D = [100, 500]
+ns_2D = [100, 500]
+
+#
 if dim == 1:
     us = us_1D
+    us_count = len(us)
+
+    ztpers = zeta_t_perp_1D
+    ztpers_count = 1
+
+    ns = ns_1D
+    ns_count = len(ns)
+
+    zeta_t_roots_size = (us_count, ns_count, lambs_count + 1, 2, zeta_sp_steps)
 else:
     us = us_2D
+    us_count = 1
+
+    ztpers = zeta_t_perp_2D
+    ztpers_count = len(ztpers)
+
+    ns = ns_2D
+    ns_count = len(ns)
+
+    zeta_t_roots_size = (ztpers_count, ns_count,
+                         lambs_count + 1, 2, zeta_sp_steps)
 
 # Create zeta_sp mesh
 zeta_sps = np.linspace(- zeta_sp_abs_lim, zeta_sp_abs_lim, zeta_sp_steps)
 
-# Calculate roots
-lambs_count = len(lambs)
-ns_count = len(ns)
-us_count = len(us)
-zeta_t_roots_size = (us_count, ns_count, lambs_count + 1, 2, zeta_sp_steps)
-zeta_t_roots = np.zeros(zeta_t_roots_size, dtype=np.float)
-print(zeta_t_roots_size)
-print(np.size(zeta_t_roots))
+zeta_t_roots = np.zeros(zeta_t_roots_size, dtype=np.float) * np.nan
 
-
-for u_ind in range(us_count):
-    u = us[u_ind]
 
 #     v = 1 + u * n_pi / ns + (dim - 1) * s * zeta_t_perp ** 2
 
-    for n_ind in range(ns_count):
-        n = ns[n_ind]
-        print("Processing u: %i/%i, n: %i/%i" %
-              (u_ind + 1, us_count, n_ind + 1, ns_count))
+for n_ind in range(ns_count):
+    n = ns[n_ind]
+    # print("Processing u: %i/%i, n: %i/%i" %
+    #       (u_ind + 1, us_count, n_ind + 1, ns_count))
 
-        # Check if the requested Bayes factor can be reached
-        log10_B_lims = calculate_bayes_factor_limits(
-            ns=ns, n_pi=n_pi, dim=dim, zeta_t_perp=zeta_t_perp, u=u)
-        bl_reachable = log10_B >= log10_B_lims[n_ind,
-                                               0] and log10_B <= log10_B_lims[n_ind, 1]
+    for u_ind in range(us_count):
+        u = us[u_ind]
 
-        if not bl_reachable:
-            print("Warning: The value of the Bayes factor (B = %.3g) cannot be achieved for the current parameters. Bayes factor limits: [%.3g; %.3g]"
-                  % (10 ** log10_B, 10 ** log10_B_lims[n_ind, 0], 10 ** log10_B_lims[n_ind, 1]))
-            continue
+        for ztper_ind in range(ztpers_count):
+            ztper = ztpers[ztper_ind]
 
-        for zeta_ind in range(zeta_sp_steps):
-            zeta_sp = zeta_sps[zeta_ind]
+            # Check if the requested Bayes factor can be reached
+            log10_B_lims = calculate_bayes_factor_limits(
+                ns=ns, n_pi=n_pi, dim=dim, zeta_t_perp=ztper, u=u)
+            bl_reachable = log10_B >= log10_B_lims[n_ind,
+                                                   0] and log10_B <= log10_B_lims[n_ind, 1]
 
-            # Fixed-lambda models
-            for lamb_ind in range(lambs_count):
-                lamb = lambs[lamb_ind]
+            if not bl_reachable:
+                print("Warning: The value of the Bayes factor (B = %.3g) cannot be achieved for the current parameters. Bayes factor limits: [%.3g; %.3g]"
+                      % (10 ** log10_B, 10 ** log10_B_lims[n_ind, 0], 10 ** log10_B_lims[n_ind, 1]))
+                continue
 
-                cur_roots = calculate_zeta_t_roots(zeta_sp=zeta_sp, lamb=lamb, n=n, n_pi=n_pi, B=B,
-                                                   dim=dim, zeta_t_perp=zeta_t_perp, u=u)
+            for zeta_ind in range(zeta_sp_steps):
+                zeta_sp = zeta_sps[zeta_ind]
+
+                # Fixed-lambda
+                for lamb_ind in range(lambs_count):
+                    lamb = lambs[lamb_ind]
+
+                    cur_roots = calculate_zeta_t_roots(
+                        zeta_sp=zeta_sp, lamb=lamb, n=n, n_pi=n_pi, B=B, dim=dim, zeta_t_perp=ztper, u=u)
+
+                    # Save
+                    if dim == 1:
+                        zeta_t_roots[u_ind, n_ind, lamb_ind,
+                                     0, zeta_ind] = cur_roots[0]
+                        zeta_t_roots[u_ind, n_ind, lamb_ind,
+                                     1, zeta_ind] = cur_roots[1]
+                    else:
+                        zeta_t_roots[ztper_ind, n_ind, lamb_ind,
+                                     0, zeta_ind] = cur_roots[0]
+                        zeta_t_roots[ztper_ind, n_ind, lamb_ind,
+                                     1, zeta_ind] = cur_roots[1]
+
+                # Marginalized
+                cur_roots = find_marginalized_zeta_t_roots(
+                    zeta_sp=[zeta_sp], n=n, n_pi=n_pi, B=B, u=u, dim=dim, zeta_t_perp=ztper)
+
                 # Save
-                zeta_t_roots[u_ind, n_ind, lamb_ind,
-                             0, zeta_ind] = cur_roots[0]
-                zeta_t_roots[u_ind, n_ind, lamb_ind,
-                             1, zeta_ind] = cur_roots[1]
-
-            # Marginalized
-            cur_roots = find_marginalized_zeta_t_roots(
-                zeta_sp=[zeta_sp], n=n, n_pi=n_pi, B=B, u=u, dim=dim, zeta_t_perp=zeta_t_perp)
-            # Save
-            zeta_t_roots[u_ind, n_ind, lambs_count, 0, zeta_ind] = cur_roots[0]
-            zeta_t_roots[u_ind, n_ind, lambs_count, 1, zeta_ind] = cur_roots[1]
+                if dim == 1:
+                    zeta_t_roots[u_ind, n_ind, lambs_count,
+                                 0, zeta_ind] = cur_roots[0]
+                    zeta_t_roots[u_ind, n_ind, lambs_count,
+                                 1, zeta_ind] = cur_roots[1]
+                else:
+                    zeta_t_roots[ztper_ind, n_ind, lambs_count,
+                                 0, zeta_ind] = cur_roots[0]
+                    zeta_t_roots[ztper_ind, n_ind, lambs_count,
+                                 1, zeta_ind] = cur_roots[1]
 
 print("Calculation terminated")
+# print(zeta_t_roots)
 
 # print(zeta_t_roots)
 
 # %% plot
 if dim == 1:
     plot_K_L(zeta_sps=zeta_sps, zeta_t_roots=zeta_t_roots,
-             ns=ns, us=us, dim=dim, zeta_t_perp=zeta_t_perp)
+             ns=ns, us=us, dim=dim, zeta_t_perp=ztpers)
 else:
     plot_K_L_2D(zeta_sps=zeta_sps, zeta_t_roots=zeta_t_roots,
-                ns=ns, us=us, dim=dim, zeta_t_perp=zeta_t_perp)
+                ns=ns, us=us, dim=dim, ztpers=ztpers)
 
 
 #
--- analytical_bayes_factor/plot_K_L_2D.py
+++ analytical_bayes_factor/plot_K_L_2D.py
@@ -7,7 +7,7 @@ import numpy as np
 from set_figure_size import set_figure_size
 
 
-def plot_K_L_2D(zeta_sps, zeta_t_roots, ns, us, dim, zeta_t_perp):
+def plot_K_L_2D(zeta_sps, zeta_t_roots, ns, us, dim, ztpers):
     """
     Make the parametric plot of the local Bayes factor
     """
@@ -23,7 +23,8 @@ def plot_K_L_2D(zeta_sps, zeta_t_roots, ns, us, dim, zeta_t_perp):
     lambs_count = np.size(zeta_t_roots, 2)
     ns_count = len(ns)
     us_count = len(us)
-    rows = 2
+    ztpers_count = len(ztpers)
+    rows = ztpers_count
 
     fig = set_figure_size(
         num=1, rows=rows, page_width_frac=0.5, height_factor=1.0)
@@ -31,50 +32,52 @@ def plot_K_L_2D(zeta_sps, zeta_t_roots, ns, us, dim, zeta_t_perp):
 
     # Print dimensions
     dim_str = "%iD" % (dim)
-    if dim == 2:
-        dim_str += " ($\zeta_{t\perp}=%.2f$)" % (zeta_t_perp)
-    # fig.text(0.01, 0.97, dim_str, fontsize = font_size + 2, weight = 'bold')
+    # if dim == 2:
+    #     dim_str += " ($\zeta_{t\perp}=%.2f$)" % (zeta_t_perp)
+    # # fig.text(0.01, 0.97, dim_str, fontsize = font_size + 2, weight = 'bold')
     fig.suptitle(dim_str, fontsize=font_size + 2)
 
     count = 0
-    for u_ind in range(us_count):
-        u = us[u_ind]
+    for ztpers_ind in range(ztpers_count):
+        ztper = ztpers[ztpers_ind]
 
         # y title
         if dim == 1:
             y_str = '$\zeta_\mathrm{t}$ $(u='
         elif dim == 2:
-            y_str = '$\zeta_{\mathrm{t}\parallel}$ $(u_\perp='
-        y_str += '%.1f)$' % (u)
-        axarr[u_ind, 0].set_ylabel(y_str)
+            y_str = '$\zeta_{\mathrm{t}\parallel}$ $(|\zeta_{t\perp}|='
+        y_str += '%.1f)$' % (ztper)
+        axarr[ztpers_ind, 0].set_ylabel(y_str)
 
         for n_ind in range(ns_count):
-            ax = axarr[u_ind, n_ind]
+            ax = axarr[ztpers_ind, n_ind]
             n = ns[n_ind]
 
             # Plot fixed-lambda
             for lamb_ind in range(lambs_count - 1):
-                ax.fill_between(zeta_sps, zeta_t_roots[u_ind, n_ind, lamb_ind, 0, :],
-                                zeta_t_roots[u_ind, n_ind, lamb_ind, 1, :],
+                ax.fill_between(zeta_sps, zeta_t_roots[ztpers_ind, n_ind, lamb_ind, 0, :],
+                                zeta_t_roots[ztpers_ind,
+                                             n_ind, lamb_ind, 1, :],
                                 color=color_sequence[lamb_ind], alpha=alpha)
 
             lamb_ind = lambs_count - 1
             # Positive zeta_t branch
             branch_ind = 0
-            y_axis = zeta_t_roots[u_ind, n_ind, lamb_ind, branch_ind, :]
+            y_axis = zeta_t_roots[ztpers_ind, n_ind, lamb_ind, branch_ind, :]
+            # print(y_axis)
             ax.plot(zeta_sps, y_axis, color=color_sequence[lamb_ind])
 
             # Negative zeta_t branch
             branch_ind = 1
-            y_axis = zeta_t_roots[u_ind, n_ind, lamb_ind, branch_ind, :]
+            y_axis = zeta_t_roots[ztpers_ind, n_ind, lamb_ind, branch_ind, :]
             ax.plot(zeta_sps, y_axis, color=color_sequence[lamb_ind])
 
             # Adjust
             # plt.axis([zeta_t_points[0] * 1.05, zeta_t_points[-1] * 1.05, -2, 2])
-            if u_ind == us_count - 1:
+            if ztpers_ind == ztpers_count - 1:
                 ax.set_xlabel('$\zeta_\mathrm{sp}$')
 
-            if u_ind == 0:
+            if ztpers_ind == 0:
                 ax.set_title("n = %i" % (n))
 
             # Add a label to each plot
