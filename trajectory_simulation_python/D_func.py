4c85ffc2f19898f94b4370bc81d2adc5b58d6b56
Alexander Serov <asserov@gmail.com>
2018-06-07 16:04:56 +0200

    Added the possibility to simulated a trajectory with an orthogonal force

--- trajectory_simulation_python/D_func.py
+++ trajectory_simulation_python/D_func.py
@@ -1,116 +1,104 @@
-## The function calculates local diffusivity D in um^2/s and b'b in um/s
+# The function calculates local diffusivity D in um^2/s and b'b in um/s
 
 
 import math
 import numpy as np
-from constants import D_0, D_ratio, L, q, well_radius
+from constants import *
 
 
 def D_func(D_case_number, x, y, L):
 
-	# Load constants
-	D_min = D_0
-	D_max = D_0 * D_ratio
-	
+    # Load constants
+    D_min = D_0
+    D_max = D_0 * D_ratio
 
-	# Initialize
-	period = L/q
-	D_grad_abs = 2.0 * q / L * D_0 * (D_ratio - 1.0)
+    # Initialize
+    period = L / q
+    D_grad_abs = 2.0 * q / L * D_0 * (D_ratio - 1.0)
 
-	def rel_x_in_period(x):
-		return (x % period) / period
+    def rel_x_in_period(x):
+        return (x % period) / period
 
-	## Select f function
-	def D_func_local(x):
-		
-		# Saw-tooth
-		if D_case_number == 1:
-			return D_max - (D_max - D_min) * np.abs(rel_x_in_period(x) - 0.5) * 2.0
-		
-		# Round well
-		elif D_case_number == 2:
-			return D_min + (np.sqrt((x - L / 2.0)**2 + (y - L/2.0)**2) >= well_radius).astype(np.float16) * (D_max - D_min)
+    # Select f function
+    def D_func_local(x):
 
+        # Saw-tooth
+        if D_case_number == 1:
+            return D_max - (D_max - D_min) * np.abs(rel_x_in_period(x) - 0.5) * 2.0
 
-		# # Linear
-		# elif D_case_number == 2:
-		# 	return D_0 * (2.0 + k * x)
-		
-		# # Diffusivity jump
-		# elif D_case_number == 3:
-		# 	D_min = 1.0
-		# 	D_max = 2.0
-		# 	return D_min * int(x<0.0) + D_max * int(x>=0.0)
-		# elif D_case_number == 4: # Linearly growing 
-		# 	D_min = 1.0
-		# 	D_max = 2.0
-		# 	alpha = 7.0 * math.log(2.0)
-		# 	A = (D_max - D_min) / (math.exp(alpha) - 1.0)
-		# 	B = D_min - A
-		# 	return A * math.exp(alpha * (x + 0.5)) + B
-		# elif D_case_number == 5: # Linearly growing
-		# 	D_shift = 1.5
-		# 	D_slope = 1.0
-		# 	return D_shift + D_slope * x/float(L)
+        # Round well
+        elif D_case_number == 2:
+            return D_min + (np.sqrt((x - L / 2.0)**2 + (y - L / 2.0)**2) >= well_radius).astype(np.float16) * (D_max - D_min)
 
-		# elif D_case_number == 6: # Oscillating
-		# 	return D_0/2.0 * (2.0 + np.sin(np.pi * omega * x))
+        # # Linear
+        # elif D_case_number == 2:
+        # 	return D_0 * (2.0 + k * x)
 
-		# # Saw-tooth, two parts 
-		# elif D_case_number == 7:
-		# 	return D_0 * (1.0 + k * (L/2.0 - np.abs(x)))
-			
+        # # Diffusivity jump
+        # elif D_case_number == 3:
+        # 	D_min = 1.0
+        # 	D_max = 2.0
+        # 	return D_min * int(x<0.0) + D_max * int(x>=0.0)
+        # elif D_case_number == 4: # Linearly growing
+        # 	D_min = 1.0
+        # 	D_max = 2.0
+        # 	alpha = 7.0 * math.log(2.0)
+        # 	A = (D_max - D_min) / (math.exp(alpha) - 1.0)
+        # 	B = D_min - A
+        # 	return A * math.exp(alpha * (x + 0.5)) + B
+        # elif D_case_number == 5: # Linearly growing
+        # 	D_shift = 1.5
+        # 	D_slope = 1.0
+        # 	return D_shift + D_slope * x/float(L)
 
+        # elif D_case_number == 6: # Oscillating
+        # 	return D_0/2.0 * (2.0 + np.sin(np.pi * omega * x))
 
-	## Select D' function
-	def D_prime_func(x):
-		# Saw-tooth
-		if D_case_number == 1:	
-			sign = (rel_x_in_period(x) <= 0.5) * 2.0 - 1.0
-			return D_grad_abs * sign
+        # # Saw-tooth, two parts
+        # elif D_case_number == 7:
+        # 	return D_0 * (1.0 + k * (L/2.0 - np.abs(x)))
 
-		# Round well
-		elif D_case_number == 2:
-			return x * 0.0
-		
-		# # Linear
-		# elif D_case_number == 2:
-		# 	return D_0 * k
+    # Select D' function
 
-		# elif D_case_number == 3:
-		# 	D_min = 1.0
-		# 	D_max = 2.0
-		# 	return 0.0 * x
-		# elif D_case_number == 4: # Linearly growing force 
-		# 	D_min = 1.0
-		# 	D_max = 2.0
-		# 	alpha = 7.0 * math.log(2.0)
-		# 	A = (D_max - D_min) / (math.exp(alpha) - 1.0)
-		# 	B = D_min - A
-		# 	return alpha * A * math.exp(alpha * (x + 0.5))
-		# elif D_case_number == 5: # Linearly growing
-		# 	D_shift = 1.5
-		# 	D_slope = 1.0
-		# 	return D_slope * 1.0 / float(L)
+    def D_prime_func(x):
+        # Saw-tooth
+        if D_case_number == 1:
+            sign = (rel_x_in_period(x) <= 0.5) * 2.0 - 1.0
+            return D_grad_abs * sign
 
-		# elif D_case_number == 6: # Oscillating
-		# 	return D_0/2.0 * np.pi * omega * np.cos(np.pi * omega * x)
+        # Round well
+        elif D_case_number == 2:
+            return x * 0.0
 
-		# # Saw-tooth, two parts 
-		# elif D_case_number == 7:
-		# 	return - D_0 * k * np.sign(x)
+        # # Linear
+        # elif D_case_number == 2:
+        # 	return D_0 * k
 
+        # elif D_case_number == 3:
+        # 	D_min = 1.0
+        # 	D_max = 2.0
+        # 	return 0.0 * x
+        # elif D_case_number == 4: # Linearly growing force
+        # 	D_min = 1.0
+        # 	D_max = 2.0
+        # 	alpha = 7.0 * math.log(2.0)
+        # 	A = (D_max - D_min) / (math.exp(alpha) - 1.0)
+        # 	B = D_min - A
+        # 	return alpha * A * math.exp(alpha * (x + 0.5))
+        # elif D_case_number == 5: # Linearly growing
+        # 	D_shift = 1.5
+        # 	D_slope = 1.0
+        # 	return D_slope * 1.0 / float(L)
 
-	## Calculate the result
-	D_func_value = D_func_local(x);
-	D_prime_func_value = D_prime_func(x);
+        # elif D_case_number == 6: # Oscillating
+        # 	return D_0/2.0 * np.pi * omega * np.cos(np.pi * omega * x)
 
+        # # Saw-tooth, two parts
+        # elif D_case_number == 7:
+        # 	return - D_0 * k * np.sign(x)
 
-	return [D_func_value, D_prime_func_value]
+    # Calculate the result
+    D_func_value = D_func_local(x)
+    D_prime_func_value = D_prime_func(x)
 
-
-
-
-
-
-
+    return [D_func_value, D_prime_func_value]
--- trajectory_simulation_python/constants.py
+++ trajectory_simulation_python/constants.py
@@ -1,50 +1,58 @@
 
 
+import numpy as np
 
-## Constants
+# Constants
 version = 20180411
 
-dim = 2	# number of dimensions (1 or 2)
-L = 1.0	# linear size of the system, in um 
-x_min = 0.0	# in um
-x_max = L	# in um
-t_step = 0.04 # in seconds
-
+dim = 2  # number of dimensions (1 or 2)
+L = 1.0  # linear size of the system, in um
+x_min = 0.0  # in um
+x_max = L  # in um
+t_step = 0.04  # in seconds
 
 
 # Diffusivity constants
-D_0 = 0.01	# in um^2/s
-q = 1	# number of D peaks
-gamma_drag = 400.0	# viscous drag, in fN * s / um
+D_0 = 0.01  # in um^2/s
+q = 1  # number of D peaks
+gamma_drag = 400.0  # viscous drag, in fN * s / um
 
 
 # Total force constants
-# >> saw-tooth case for main check <<
-# D_case = 1
-# D_ratio = 2.0	# Ratio between max and min of the diffusivity
-# ksi_range = (-25.0, 25.0)	# range of ratios of the total force to diffusivity gradient
-# ksi_step = 0.5	# sampling step in the interval
-# trials = 1000	# 1000
-# N = int(1.0e4) # int(1.0e6) or int(1.0e5)
+# >> saw - tooth case for main check <<
+D_case = 1
+D_ratio = 2.0  # Ratio between max and min of the diffusivity
+trials = 100  # 1000
+N = int(1.0e4)  # 10^4 used in the article
 
-# >> round-well case for gradient jump check <<
-D_case = 2
-D_ratio = 10.0	# Ratio between max and min of the diffusivity
-well_radius = L / 6.0
-ksi_range = (0.5, 25.0)	# range of ratios of the total force to diffusivity gradient
-ksi_step = 100.0	# sampling step in the interval
-trials = 1
-N = int(1.0e5) # int(1.0e6) or int(1.0e5)
+# # >> round-well case for gradient jump check <<
+# D_case = 2
+# D_ratio = 10.0  # Ratio between max and min of the diffusivity
+# well_radius = L / 6.0
+# ksi_range = (-25.0, 25.0)  # range of ratios of the total force to diffusivity gradient
+# ksi_step = 0.5  # sampling step in the interval
+
+# the orthogonal force component IF the boolean flag is passed True to the program
+bl_orthogonal_force = True
+zeta_t_y = 0.1
+
+# if bl_orthogonal_force:
+#     ksi_range = (-10.0, 10.0)  # range of ratios of the total force to diffusivity gradient
+#     ksi_step = 0.2  # sampling step in the interval
+# else:
+ksi_range = (-25.0, 25.0)  # range of ratios of the total force to diffusivity gradient
+ksi_step = 0.5  # sampling step in the interval
 
 
 # Simulation parameters
 progress_update_interval = 100.0
-internal_steps_number = 100 # Integer. How many intermediate smaller steps are made before the next point is saved
-k = 2.0 * q / L * (D_ratio - 1.0)	# D'/D
-D_grad_abs = k * D_0	# absolute value of the diffusivity gradient used for force calculations
+# Integer. How many intermediate smaller steps are made before the next point is saved
+internal_steps_number = 100
+k = 2.0 * q / L * (D_ratio - 1.0)  # D'/D
+D_grad_abs = k * D_0  # absolute value of the diffusivity gradient used for force calculations
+
 
 # Batch parameters
-
 sleep_time = 0.2
 logs_folder = "./logs/"
 output_folder = "./output/"
@@ -52,7 +60,7 @@ args_file = "./arguments.dat"
 args_lock = "./arguments.lock"
 lock_timeout = 300
 
-jobs_count_tars = 134	# 132+12
+jobs_count_tars = 134  # 132+12
 jobs_count_t_bayes = 132
 jobs_count_onsager = 1
 manager_script = "job_manager.py"
@@ -64,20 +72,8 @@ CSV_DELIMITER = ';'
 
 # k = 100.0 # diffusivity gradient, D'/D, in um^-1
 
-max_D_case = 7;
+max_D_case = 7
 
-max_f_case = 8;
+max_f_case = 8
 
-str_mode = 'periodic' #    bc_type = ENUM_BC_PERIODIC;
-
-
-
-
-
-
-
-
-
-
-
-
+str_mode = 'periodic'  # bc_type = ENUM_BC_PERIODIC;
